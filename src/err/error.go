7de529d74e249dab8deb9ecea56909b33c12f140
luccChagas <luccaa.chagas23@gmail.com>
2022-09-20 13:53:49 -0300

    setup database and add erro layer

---
+++ src/err/error.go
@@ -0,0 +1,14 @@
+package err
+
+type Handling struct {
+	Error   string `json:"error,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+const (
+	ERR_BIND_OBJECT = "Error to Bind Book Object"
+	ERR_SERVICE     = "Service Error Execution"
+	ERR_REPO        = "Repository Error Execution"
+	ERR_DB_CONN     = "database.Conn Error to retrive db:"
+	ERR_EXEC_QUERY  = "A error ocurred to Exec Query:"
+)
